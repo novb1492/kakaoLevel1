20fa0615d783c6b5e0bcd3266f931e04c5e61a84
novb1492 <novb1492@naver.com>
2022-03-12 16:05:43 +0900

    조금 생각하게되네

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,18 +11,43 @@ public class App {
     public static String solution(int[] numbers, String hand) {
         String answer = "";
         String usingHand="";
-        Map<String,Integer>useingHandAndNum=new HashMap<>();
-        for(int num:numbers){
-            if(num==1||num==4||num==7){
-                answer=answer+"L";
-                useingHandAndNum.put("L", num);
-            }else if(num==3||num==6||num==9){
-                answer=answer+"R";
-                useingHandAndNum.put("R", num);
+        Map<String,String>xy=new HashMap<>();
+        int x=0;
+        int y=0;
+        for(int i=1;i<=12;i++){
+            String num=Integer.toString(i);
+            if(num.equals("10")){
+                num="*";
+            }else if(num.equals("11")){
+                num="0";
+            }else if(num.equals("12")){
+                num="#";
+            }
+            xy.put(num, x+","+y);
+            if(i%3==0){
+                y+=1;
+                x=0;
             }else{
-                answer=answer+"m";
+                x+=1;
             }
         }
+        Map<String,String>useingHandAndNum=new HashMap<>();
+        useingHandAndNum.put("L", "*");
+        useingHandAndNum.put("R", "#");
+        for(int num:numbers){
+            if(num==1||num==4||num==7){
+                useingHandAndNum.replace("L", Integer.toString(num));
+            }else if(num==3||num==6||num==9){
+                answer=answer+"R";
+                useingHandAndNum.replace("R", Integer.toString(num));
+            }else{
+                String[] leftXY=xy.get(useingHandAndNum.get("L")).split(",");
+                String[] rightXY=xy.get(useingHandAndNum.get("R")).split(",");
+                String[] numXY=xy.get(Integer.toString(num)).split(",");
+                System.out.println(Arrays.toString(numXY));             
+            }
+        }
+        
         return answer;
     }
     
