71c764fc468655f3ca63bc47b0383e97a9e8b1ef
novb1492 <novb1492@naver.com>
2022-03-12 14:44:45 +0900

    이지

--- src/App.java
+++ src/App.java
@@ -1,74 +1,75 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String id="=.=";//...!@BaT#*..y.abcdefghijklm";
+        String id="1zerotwozero3";
         System.out.println(solution(id));
     }
-    public static String solution(String new_id) {
-        String answer = new_id.toLowerCase();
-        for(int i=0;i<=5;i++){
-            if(i==0){
-                for(char spelling:answer.toCharArray()){
-                    int ascode=(int)spelling;
-                    if((ascode>=48&&ascode<=57)||ascode==45||ascode==95||ascode==46||(ascode>=97&&ascode<=122)){
-                        continue;
-
-                    }else{
-                        String a=""+spelling;
-                        answer=answer.replace(a,"");
-                    }
-                }
-                //System.out.println(answer);
-            }else if(i==1){
-                char[] arr=answer.toCharArray();
-                int len=arr.length;
-                for(int ii=0;ii<len;ii++){
-                    int ascode=(int)arr[ii];
-                    if(ii==len-1){
+    public static int solution(String s) {
+        int answer = 0;
+        try {
+            answer=Integer.parseInt(s);
+        } catch (Exception e) {
+            char[] arr=s.toCharArray();
+            int len =arr.length; 
+            String nums="";    
+            String num="";     
+            for(int i=0;i<len;i++){
+               // System.out.println(arr[i]);
+                if(arr[i]>=97&&arr[i]<=122){
+                    num=num+arr[i];
+                    switch (num) {
+                        case "zero":
+                        nums=nums+0;
+                        num="";
                         break;
-                    }else if(ascode==46&&arr[ii+1]==46){
-                        arr[ii]='#';
-                        continue;
-
+                        case "one":
+                            nums=nums+1;
+                            num="";
+                            break;
+                        case "two":
+                            nums=nums+2;
+                            num="";
+                            break;
+                        case "three":
+                            nums=nums+3;
+                            num="";
+                            break;
+                        case "four":
+                            nums=nums+4;
+                            num="";
+                            break;
+                        case "five":
+                            nums=nums+5;
+                            num="";
+                            break;
+                        case "six":
+                            nums=nums+6;
+                            num="";
+                            break;
+                        case "seven":
+                            nums=nums+7;
+                            num="";
+                            break;
+                        case "eight":
+                            nums=nums+8;
+                            num="";
+                            break;
+                        case "nine":
+                            nums=nums+9;
+                            num="";
+                            break;
+                        default:
+                            break;
                     }
-                }
-                answer=new String(arr).replace("#", "");
-                arr=answer.toCharArray();
-                if(arr[0]==46){
-                    arr[0]='#';
-                }
-                if(arr[arr.length-1]==46){
-                    arr[arr.length-1]='#';
-                }
-                answer=new String(arr).replace("#", "");
-               // System.out.println(answer);
-            }else if(i==2){
-                if(answer.isBlank()){
-                    answer="a";
-                }
-                if(answer.length()>=16){
-                    answer=answer.substring(0, 15);
-                    char[] arr=answer.toCharArray();
-                    if(arr[arr.length-1]==46){
-                        arr[arr.length-1]='#';
-                    }
-                    answer=new String(arr).replace("#", "");
-                }else if(answer.length()<=2){
-                   char[] arr=answer.toCharArray();
-                   while(true){
-                       answer=answer+arr[arr.length-1];
-                       if(answer.length()>=3){
-                        break;
-                       }
-                   }
+                }else{
+                    nums=nums+arr[i];
                 }
             }
+            answer=Integer.parseInt(nums);
         }
-        //System.out.println(new_id.toLowerCase());
         return answer;
     }
+    
     
 }
