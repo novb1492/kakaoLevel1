bfff35136edb5c090523388636fcf14b7eb6c185
novb1492 <novb1492@naver.com>
2022-03-12 13:21:10 +0900

    로또의최고 순위와 최저순위

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+카카오 코딩 테스트 레벨 1 모음 풀어오기  
--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] lottos={44, 1, 0, 0, 31, 25};
+        int[] win_nums={31, 10, 45, 1, 6, 19};
+        System.out.println(Arrays.toString(solution(lottos, win_nums)));
+    }
+    public static int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        return answer;
     }
 }
