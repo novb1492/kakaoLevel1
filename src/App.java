16cba0d95a3170f21966129aff15a44217f9aaa8
novb1492 <novb1492@naver.com>
2022-03-12 22:33:35 +0900

    와소수점 비교 이퀄스였다

--- src/App.java
+++ src/App.java
@@ -10,7 +10,7 @@ import java.util.Map.Entry;
 public class App {
     public static void main(String[] args) throws Exception {
         int n=5;
-        int[] stages={2, 1, 2, 5, 2, 4, 3, 3};
+        int[] stages={2, 1, 2, 6, 2, 4, 3, 3};
         System.out.println(Arrays.toString(solution(n,stages)));
     }
     public static int[] solution(int N, int[] stages) {
@@ -33,19 +33,22 @@ public class App {
         int postion=0;
         for(Entry<Integer, Double> stageAndPerson:stageAndPersons.entrySet()){
             System.out.println(stageAndPerson.getKey());
-           // System.out.println("value:"+stageAndPerson.getValue());
+            System.out.println("value:"+stageAndPerson.getValue());
             for(Entry<Integer, Double> s:stageAndPersons.entrySet()){
                 if(s.getKey()==stageAndPerson.getKey()){
                     continue;
                 }
-                System.out.println("value:"+stageAndPerson.getValue());
+                System.out.println("skey: "+s.getKey());
                 System.out.println("value2:"+s.getValue());
+       
                 if(stageAndPerson.getValue()<s.getValue()){
                     postion+=1;
+                }else if(stageAndPerson.getValue().equals(s.getValue())){
+                    System.out.println("b");
                 }
             }
-            System.out.println("po: "+postion);
-            //answer[postion]=stageAndPerson.getKey();
+           // System.out.println("po: "+postion);
+           // answer[postion]=stageAndPerson.getKey();
             postion=0;
         }
         return answer;
