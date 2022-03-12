1902bcd6df8fa0928f0eb2463b957188297becd8
novb1492 <novb1492@naver.com>
2022-03-12 13:37:22 +0900

    신규아이디 추천

--- src/App.java
+++ src/App.java
@@ -2,52 +2,11 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] lottos={45, 4, 35, 20, 3, 9};//{0, 0, 0, 0, 0, 0};//{44, 1, 0, 0, 31, 25};
-        int[] win_nums={20, 9, 3, 45, 4, 35};//{38, 19, 20, 40, 15, 25};//{31, 10, 45, 1, 6, 19};
-
-        System.out.println(Arrays.toString(solution(lottos, win_nums)));
+        String id="...!@BaT#*..y.abcdefghijklm";
+        System.out.println(solution(id));
     }
-    public static int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int rigtNumCount=0;
-        int zeroNumCount=0;
-        for(int lotto:lottos){
-            if(lotto==0){
-                zeroNumCount+=1;
-                continue;
-            }
-            for(int winNum:win_nums){
-                if(lotto==winNum){
-                    rigtNumCount+=1;
-                    break;
-                }
-            }
-        }
-       // System.out.println("rigth: "+rigtNumCount);
-        //System.out.println("zero: "+zeroNumCount);
-        int max=rigtNumCount+zeroNumCount;
-        int min=rigtNumCount;
-        //System.out.println("max: "+max);
-        //System.out.println("min: "+min);
-        answer[0]=getRank(max);
-        answer[1]=getRank(min);
+    public static String solution(String new_id) {
+        String answer = "";
         return answer;
-    }
-    private static int getRank(int num) {
-        switch (num) {
-            case 6:
-                return 1;
-            case 5:
-                return 2;
-            case 4:
-                return 3;
-            case 3:
-                return 4;
-            case 2:
-                return 5;
-        
-            default:
-                return 6;
-        }
     }
 }
