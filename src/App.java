e5f64c5c85fda8dac2afa440d57cd573270ef978
novb1492 <novb1492@naver.com>
2022-03-12 21:42:21 +0900

    다시배열만들어야함

--- src/App.java
+++ src/App.java
@@ -10,23 +10,23 @@ import java.util.Map.Entry;
 public class App {
     public static void main(String[] args) throws Exception {
         int n=5;
-        int[] stages={2, 1, 2, 6, 2, 4, 3, 3};
+        int[] stages={2, 1, 2, 4, 2, 4, 3, 3};
         System.out.println(Arrays.toString(solution(n,stages)));
     }
     public static int[] solution(int N, int[] stages) {
         Map<Integer,String>stageAndPersons=new HashMap<>();
-        for(int stage:stages){
-            String[] personAndCount=Optional.ofNullable(stageAndPersons.get(stage)).orElseGet(()->"0,0").split(",");
+        for(int i=1;i<=N;i++){
+            String[] personAndCount=Optional.ofNullable(stageAndPersons.get(i)).orElseGet(()->"0,0").split(",");
             int person=Integer.parseInt(personAndCount[0]);
             person+=1;
             int count=0;
-            for(int i:stages){
-                if(i>=stage){
+            for(int ii:stages){
+                if(ii>=i){
                     count+=1;
                 }
             }
             String personAndCounts=person+","+count;
-            stageAndPersons.put(stage, personAndCounts);
+            stageAndPersons.put(i, personAndCounts);
         }
         System.out.println(stageAndPersons.toString());
         for(Entry<Integer, String> stageAndPerson:stageAndPersons.entrySet()){
@@ -35,7 +35,7 @@ public class App {
             int count=Integer.parseInt(personAndCount[1]);
             stageAndPersons.replace(stageAndPerson.getKey(),Double.toString((double)person/count));
         }
-        System.out.println(stageAndPersons.toString());
+        //System.out.println(stageAndPersons.toString());
         int[] answer = new int[N];
         return answer;
     }
