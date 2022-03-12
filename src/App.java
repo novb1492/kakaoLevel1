1d107d17a34540acc7bbe1b3ce967e157fb08ea3
novb1492 <novb1492@naver.com>
2022-03-12 20:27:06 +0900

    실패율

--- src/App.java
+++ src/App.java
@@ -8,43 +8,12 @@ import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[][] board={{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[]moves={1,5,3,5,1,2,1,4};
-        System.out.println(solution(board,moves));
+        int n=5;
+        int[] stages={2, 1, 2, 6, 2, 4, 3, 3};
+        System.out.println(Arrays.toString(solution(n,stages)));
     }
-    public static int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        Stack<Integer>carts=new Stack<>();
-        for(int move:moves){
-            int num=0;
-            for(int i=0;i<board.length;i++){
-                num=board[i][move-1];
-                if(num!=0){
-                    board[i][move-1]=0;
-                    break;
-                }
-            }
-            if(num!=0){
-                try {
-                    int beforeNum=carts.peek();
-                    if(beforeNum==num){
-                        answer+=2;
-                        carts.pop();
-                    }else{
-                        carts.push(num);
-                    }
-                } catch (Exception e) {
-                   carts.push(num);
-                }
-            }
-           
-        }
-        System.out.println(carts.toString());
-        //15351214
-        //01130302
-
-        
-        
+    public static int[] solution(int N, int[] stages) {
+        int[] answer = {};
         return answer;
     }
     
