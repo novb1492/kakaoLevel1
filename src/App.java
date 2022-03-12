1c25c5b75ab6019f33967db9fa296a4c20cb67cc
novb1492 <novb1492@naver.com>
2022-03-12 21:52:58 +0900

    이렇게 잡고

--- src/App.java
+++ src/App.java
@@ -10,23 +10,33 @@ import java.util.Map.Entry;
 public class App {
     public static void main(String[] args) throws Exception {
         int n=5;
-        int[] stages={2, 1, 2, 4, 2, 4, 3, 3};
+        int[] stages={2, 1, 2, 6, 2, 4, 3, 3};
         System.out.println(Arrays.toString(solution(n,stages)));
     }
     public static int[] solution(int N, int[] stages) {
         Map<Integer,String>stageAndPersons=new HashMap<>();
         for(int i=1;i<=N;i++){
-            String[] personAndCount=Optional.ofNullable(stageAndPersons.get(i)).orElseGet(()->"0,0").split(",");
-            int person=Integer.parseInt(personAndCount[0]);
-            person+=1;
             int count=0;
+            int totalCount=0;
+            int maxCount=0;
             for(int ii:stages){
                 if(ii>=i){
+                    totalCount+=1;
+                }
+                if(ii==i){
                     count+=1;
                 }
+                if(ii==N+1){
+                    maxCount+=1;
+                }
             }
-            String personAndCounts=person+","+count;
-            stageAndPersons.put(i, personAndCounts);
+            if(i==N){
+                String personAndCounts=maxCount+","+totalCount;
+                stageAndPersons.put(i, personAndCounts);
+            }else{
+                String personAndCounts=count+","+totalCount;
+                stageAndPersons.put(i, personAndCounts);
+            }
         }
         System.out.println(stageAndPersons.toString());
         for(Entry<Integer, String> stageAndPerson:stageAndPersons.entrySet()){
@@ -35,7 +45,7 @@ public class App {
             int count=Integer.parseInt(personAndCount[1]);
             stageAndPersons.replace(stageAndPerson.getKey(),Double.toString((double)person/count));
         }
-        //System.out.println(stageAndPersons.toString());
+        System.out.println(stageAndPersons.toString());
         int[] answer = new int[N];
         return answer;
     }
