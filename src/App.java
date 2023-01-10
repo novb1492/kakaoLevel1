9e2987ae3f6c7537c15e2ee89cdff15f6c9660da
김준영 <novb1492@naver.com>
2023-01-10 14:47:32 +0900

    first

--- src/App.java
+++ src/App.java
@@ -9,11 +9,28 @@ import java.util.Map.Entry;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String dartResult="1S2D*3T";
-        System.out.println(solution(dartResult));
+        System.out.println(solution("Wed",2));
     }
-    public static int solution(String dartResult) {
-        int answer = 0;
+    public static String solution(String S,int K) {
+        String answer = null;
+        String[]arr={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        int ii=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals(S)){
+                ii=i;
+                answer=arr[i];
+                break;
+            }
+        }
+        for(int i=0;i<K;i++){
+            ii+=1;
+            if(ii>=arr.length){
+                ii=0;
+                answer=arr[ii];
+            }else{
+                answer=arr[ii];   
+            }
+        }
         return answer;
     }
     
